d67de7e466a366f7e9802a40583c17f70f522908
mongoy <mongoy@rambler.ru>
2020-07-28 13:22:03 +0900

    Added: basic templates and a list of currencies; view for a list of currencies.

--- conversion/urls.py
+++ conversion/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import CurrencyListView
 
 
 urlpatterns = [
-
+    path('', CurrencyListView.as_view(), name='currency-list'),
 ]
--- conversion/views.py
+++ conversion/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import Currency
 
-# Create your views here.
+
+class CurrencyListView(ListView):
+    """ Список видов валюты """
+    model = Currency
+    request = Currency.objects.all()
+    template_name = 'conversion/currency_list.html'
